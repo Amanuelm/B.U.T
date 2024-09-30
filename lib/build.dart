b644d32364d44e1bae9c9bdde3095f4c8ba8b6cd
Amanuelm <75570250+Amanuelm@users.noreply.github.com>
2024-09-30 18:37:32 +0300

    Update build.dart

--- lib/build.dart
+++ lib/build.dart
@@ -9,8 +9,56 @@ class _BuildPageState extends State<BbuildPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Use a scrollable layout with toolbar that appears when scrolled
-      body: CustomScrollView(),
-    );
+        appBar: AppBar(title: Text('Build Page')),
+        body: Container(
+          color: Colors.green,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(children: [
+                SizedBox(width: 20),
+                Icon(
+                  Icons.account_circle_outlined,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.sports_soccer_outlined,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.sports_soccer_outlined,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.sports_baseball,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.sports_soccer_sharp,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+                SizedBox(width: 20),
+                Icon(
+                  Icons.sports_golf,
+                  color: Colors.black,
+                  size: 40.0,
+                ),
+              ]),
+              SizedBox(height: 20),
+              Image(
+                image: AssetImage('assets/images/fp.jpg'),
+              ),
+            ],
+          ),
+        ));
   }
 }
